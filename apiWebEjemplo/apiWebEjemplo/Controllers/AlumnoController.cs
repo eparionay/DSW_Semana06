be6eab6672978dba3cc9faf112b841c0dc3a2b58
EPY <erick.pariona@hotmail.com>
2024-05-04 14:14:45 -0500

    Sem

---
+++ apiWebEjemplo/apiWebEjemplo/Controllers/AlumnoController.cs
@@ -0,0 +1,122 @@
+﻿using apiWebEjemplo.Models;
+using apiWebEjemplo.services;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace apiWebEjemplo.Controllers
+{
+    public class AlumnoController : ApiController
+    {
+
+        private static readonly ILog LOG = log4net.LogManager.
+            GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        [Route("api/info")]
+        [HttpGet]
+        public String info()
+        {
+            return "Bienvenido al Curso de DSWI";
+        }
+
+        [Route("api/alumno/lista")]
+        [HttpGet]
+        public List<Alumno> lista(){
+           
+            ServiceAlumno servicioAlumno = new ServiceAlumno();
+            List<Alumno> lista = servicioAlumno.listadoAlumno("listar", 1);
+            LOG.Info("Tamano de la Lista : " + lista.Count);
+            return lista;
+        }
+
+
+        [Route("api/alumno/respuestaSW/{codigo}")]
+        [HttpGet]
+        public ResponseServer respuestaSW(int codigo)
+        {
+            ResponseServer rpta = new ResponseServer();
+            rpta.codigo = codigo;
+            rpta.mensaje = "Esto es un ejercicio de prueba";
+            return rpta;
+        }
+
+
+
+        [Route("api/alumno/registrar")]
+        [HttpPost]
+        public ResponseServer registrar(Alumno alumno)
+        {
+            ResponseServer rptaServidor = new ResponseServer();
+            ServiceAlumno servicioAlumno = new ServiceAlumno();
+            int procesar = servicioAlumno.operacionAlumno("registrar", alumno);
+            if (procesar<1){
+                rptaServidor.mensaje = "Hubo un error.";
+                rptaServidor.codigo = 0;
+            }
+            else
+            {
+                rptaServidor.mensaje = "Se ingreso correctamente";
+                rptaServidor.codigo = 1;
+            }
+            return rptaServidor;
+        }
+
+        [Route("api/alumno/actualizar")]
+        [HttpPut]
+        public ResponseServer actualizar(Alumno alumno)
+        {
+            ResponseServer rptaServidor =new ResponseServer();
+            ServiceAlumno servicioAlumno = new ServiceAlumno();
+            int actualizar = servicioAlumno.operacionAlumno("actualizar", alumno);
+            if (actualizar == 1)
+            {
+                rptaServidor.mensaje = "Se actualizo correctamente";
+                rptaServidor.codigo = 1;
+            }
+            else
+            {
+                rptaServidor.mensaje = "Hubo un error.";
+                rptaServidor.codigo = 0;
+            }
+            return rptaServidor;
+        }
+
+
+        [Route("api/alumno/eliminar/{codigo}")]
+        [HttpDelete]
+        public ResponseServer eliminar(int codigo)
+        {
+            ResponseServer rptaServidor = new ResponseServer();
+            return rptaServidor;
+        }
+
+        [Route("api/alumno/editar")]
+        [HttpPut]
+        public ResponseServer editar(Alumno alumno)
+        {
+            ResponseServer rptaServidor = new ResponseServer();
+            ServiceAlumno servicioAlumno = new ServiceAlumno();
+            int procesar = servicioAlumno.operacionAlumno("editar", alumno);
+            if (procesar < 1)
+            {
+                rptaServidor.mensaje = "Hubo un error.";
+                rptaServidor.codigo = 0;
+            }
+            else
+            {
+                rptaServidor.mensaje = "Se ingreso correctamente";
+                rptaServidor.codigo = 1;
+            }
+            return rptaServidor;
+        }
+
+
+
+    }
+}
---
+++ apiWebEjemplo/apiWebEjemplo/dao/AlumnoDao.cs
@@ -0,0 +1,18 @@
+﻿using apiWebEjemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiWebEjemplo.dao
+{
+    public interface AlumnoDao
+    {
+        // Insertar Eliminar Actualizar
+        int operacionAlumno(string indicador, Alumno objAlumno);
+
+        // ListarTodosAlumnos, ObtenerAlumnoxI
+        List<Alumno> listadoAlumno(string indicador, int codigo);
+
+    }
+}
